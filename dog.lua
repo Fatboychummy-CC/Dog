c8d6e22d320ff5c1c0b7eb8cdffdb2c82cc78d03
Matthew W <fatboychummy@gmail.com>
2023-10-01 00:41:15 -0600

    more argument checking and debugging

--- dog.lua
+++ dog.lua
@@ -17,10 +17,13 @@ local args = {...}
 local max_depth = 512
 local log_level = logging.LOG_LEVEL.INFO
 local log_window = term.current()
+local geoscanner_range = 8
+local scan = nil ---@type fun():table<integer, table> Set during initialization.
 
 local parser = simple_argparse.new_parser("dog", "Dog is a program run on mining turtles which is used to find ores and mine them. Unlike quarry programs, this program digs in a straight line down and uses either plethora's block scanner or advanced peripheral's geoscanner to detect where ores are along its path and mine to them.")
 parser.add_option("depth", "The maximum depth to dig to.", max_depth)
-parser.add_option("log-level", "The log level to use.", "INFO")
+parser.add_option("loglevel", "The log level to use.", "INFO")
+parser.add_option("georange", "The range to use for the geoscanner, if using Advanced Peripherals.", 8)
 parser.add_flag("h", "help", "Show this help message and exit.")
 
 local parsed = parser.parse(table.pack(...))
@@ -31,14 +34,24 @@ if parsed.flags.help then
   return
 end
 
-if parsed.options["log-level"] then
-  log_level = logging.LOG_LEVEL[parsed.options["log-level"]:upper()]
+if parsed.options.loglevel then
+  log_level = logging.LOG_LEVEL[parsed.options.loglevel:upper()]
+  if not log_level then
+    error("Invalid log level.", 0)
+  end
 end
-if parsed.options["depth"] then
+if parsed.options.depth then
   ---@diagnostic disable-next-line max_depth is tested right after this
-  max_depth = tonumber(parsed.options["depth"])
+  max_depth = tonumber(parsed.options.depth)
   if not max_depth then
     error("Max depth must be a number.", 0)
+  end
+end
+if parsed.options.georange then
+  ---@diagnostic disable-next-line geoscanner_range is tested right after this
+  geoscanner_range = tonumber(parsed.options.georange)
+  if not geoscanner_range then
+    error("Geo range must be a number.", 0)
   end
 end
 
@@ -54,29 +67,44 @@ do
   local scanner, geoscanner = aid.is_module_equipped("scanner"), aid.is_module_equipped("geoScanner")
 
   if scanner or geoscanner then
-    setup_context.info("Found scanner.")
+    setup_context.debug("Found scanner.")
     if scanner and geoscanner then
       error("Who ported which mod to which loader, and why?", 0)
     end
   else
     if aid.swap_module("scanner", "left") then
       scanner = "left"
-      setup_context.info("Found scanner.")
+      setup_context.debug("Found scanner.")
     elseif aid.swap_module("geoScanner", "left") then
       geoscanner = "left"
-      setup_context.info("Found geoscanner.")
+      setup_context.debug("Found geoscanner.")
     else
       error("No scanner or geoscanner found.", 0)
     end
   end
 
   if aid.is_module_equipped("pickaxe") then
-    setup_context.info("Found pickaxe.")
+    setup_context.debug("Found pickaxe.")
   else
     if aid.swap_module("pickaxe", "right") then
-      setup_context.info("Found pickaxe.")
+      setup_context.debug("Found pickaxe.")
     else
       error("No pickaxe found.", 0)
     end
   end
+
+  if scanner then
+    setup_context.debug("Using scanner on", scanner, "side.")
+    scan = function()
+      return peripheral.call(scanner, "scan")
+    end
+  end
+
+  if geoscanner then
+    setup_context.debug("Using geoscanner on", geoscanner, "side.")
+    scan = function()
+      return peripheral.call(geoscanner, "scan", geoscanner_range)
+    end
+  end
 end
+
