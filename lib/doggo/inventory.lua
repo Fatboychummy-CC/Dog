837b9172723749a0e7be943b5e258af6cddcdb7b
Matthew W <fatboychummy@gmail.com>
2025-07-17 01:01:21 -0600

    More work on the inventory library.

--- lib/doggo/inventory.lua
+++ lib/doggo/inventory.lua
@@ -39,17 +39,30 @@ local item_cache = {}
 
 
 
+--- Returns a unique identifier for an item based on its name and NBT.
+---@param item ccTweaked.turtle.turtleDetails|ccTweaked.turtle.turtleDetailsDetailed|ccTweaked.peripheral.item
+---@return string id The unique identifier for the item.
+local function get_item_id(item)
+  expect(1, item, "table")
+
+  return ("%s%s%s"):format(item.name, item.nbt and ":" or "", item.nbt or "")
+end
+
+
+
 --- Caches an item and its data.
 ---@param item ccTweaked.turtle.turtleDetailsDetailed|ccTweaked.peripheral.item
 local function cache_item(item)
   expect(1, item, "table")
 
-  if item_cache[item.name] then
+  local id = get_item_id(item)
+
+  if item_cache[id] then
     -- Item is already cached, no need to cache again.
     return
   end
 
-  item_cache[item.name] = {
+  item_cache[id] = {
     id = item.name,
     name = item.displayName,
     nbt = item.nbt,
@@ -134,7 +147,7 @@ function Inventory.dump(direction, exclude)
 
   for i = 1, 16 do
     local item = turtle.getItemDetail(i)
-    if item and not exclude_set[item.name] then
+    if item and not exclude_set[get_item_id(item)] then
       turtle.select(i)
       if direction then
         if direction == "up" then
@@ -225,7 +238,7 @@ function Inventory.interface(side)
 
     local slots = {}
     for slot, item in pairs(interface.items) do
-      if item.name == id then
+      if get_item_id(item) == id then
         table.insert(slots, slot)
       end
     end
@@ -244,7 +257,7 @@ function Inventory.interface(side)
 
     local count = 0
     for _, item in pairs(interface.items) do
-      if item.name == id then
+      if get_item_id(item) == id then
         count = count + item.count
       end
     end
@@ -266,7 +279,7 @@ function Inventory.interface(side)
 
     local collected = 0
     for slot, item in pairs(interface.items) do
-      if item.name == id then
+      if get_item_id(item) == id then
         local to_collect = math.min(item.count, count - collected)
 
         -- Transfer that many items to the first slot.
@@ -345,7 +358,78 @@ function Inventory.interface(side)
 
 
 
+  function interface.isClosed()
+    return closed
+  end
+
+
+
+  interface.rescan() -- Initial scan to populate the items list.
   return interface
+end
+
+
+
+--- We lazily assume that the recipe is argument #1, hopefully we won't change that in the future :D
+local function validate_recipe(recipe)
+  if type(recipe.recipe) ~= "table" then
+    error("Bad argument #1: missing field 'recipe'.", 3)
+  end
+  if type(recipe.items) ~= "table" then
+    error("Bad argument #1: missing field 'items'.", 3)
+  end
+
+  -- Recipe table should contain up to 3 strings of max length 3
+  for i, recipe_row in ipairs(recipe.recipe) do
+    if i > 3 then
+      error("Bad argument #1: recipe has too many rows.", 3)
+    end
+    if type(recipe_row) ~= "string" or #recipe_row > 3 then
+      error("Bad argument #1: invalid item at index " .. i .. ".", 3)
+    end
+  end
+
+  -- Items table should contain single character keys and string values
+  for key, value in pairs(recipe.items) do
+    if #key ~= 1 or type(value) ~= "string" then
+      error("Bad argument #1: invalid item '" .. key .. "'.", 3)
+    end
+  end
+end
+
+
+
+--- Gets the item costs of a recipe.
+---@param recipe Doggo.Inventory.CraftingRecipe The recipe to get the item costs for.
+---@return table<string, integer> costs A table mapping item IDs to their required counts.
+function Inventory.getRecipeCosts(recipe)
+  expect(1, recipe, "table")
+  validate_recipe(recipe)
+
+  local costs = {}
+  local char_costs = {}
+
+  -- Determine the amount of each character in the recipe.
+  for _, row in ipairs(recipe.recipe) do
+    for char in row:gmatch(".") do
+      char_costs[char] = (char_costs[char] or 0) + 1
+    end
+  end
+
+  -- Calculate the total cost for each item based on the characters.
+  -- We do this in two steps so we ignore any characters that don't exist in the items table.
+  for char, item_id in pairs(recipe.items) do
+    costs[item_id] = (costs[item_id] or 0) + (char_costs[char] or 0)
+  end
+
+  -- Prune 0 costs. This shouldn't happen, but I don't want 0 costs in the result.
+  for item_id, count in pairs(costs) do
+    if count == 0 then
+      costs[item_id] = nil
+    end
+  end
+
+  return costs
 end
 
 
@@ -353,36 +437,57 @@ end
 --- Crafts an item using the given recipe with whatever the turtle has in its inventory. Will fail if the turtle has any extra items.
 ---@param recipe Doggo.Inventory.CraftingRecipe The recipe to use for crafting.
 ---@param count integer? The number of items to craft. If nil, crafts as many as possible.
+---@param only_craft_count boolean? If true, only crafts the exact number of items specified by `count`. If requesting 5 items, but only enough to craft 3, will not craft any items.
 ---@return integer crafted The number of items successfully crafted.
-function Inventory.craft(recipe, count)
+function Inventory.craft(recipe, count, only_craft_count)
   expect(1, recipe, "table")
+  validate_recipe(recipe)
   expect(2, count, "number", "nil")
-  if type(recipe.recipe) ~= "table" then
-    error("Bad argument #1: missing field 'recipe'.", 2)
-  end
-  if type(recipe.items) ~= "table" then
-    error("Bad argument #1: missing field 'items'.", 2)
+  expect(3, only_craft_count, "boolean", "nil")
+  if count and count <= 0 then
+    return 0
   end
 
-  -- Recipe table should contain up to 3 strings of max length 3
-  for i, recipe_row in ipairs(recipe.recipe) do
-    if i > 3 then
-      error("Bad argument #1: recipe has too many rows.", 2)
-    end
-    if type(recipe_row) ~= "string" or #recipe_row > 3 then
-      error("Bad argument #1: invalid item at index " .. i .. ".", 2)
-    end
+  -- Determine the cost *per craft*.
+  local costs = Inventory.getRecipeCosts(recipe)
+  if not next(costs) then
+    ---@TODO Check: Should we just return 0 here instead?
+    error("Invalid recipe: Recipe cost is zero.", 2)
   end
 
-  -- Items table should contain single character keys and string values
-  for key, value in pairs(recipe.items) do
-    if #key ~= 1 or type(value) ~= "string" then
-      error("Bad argument #1: invalid item '" .. key .. "'.", 2)
+  local current_items = {}
+  local inventory = Inventory.getItems()
+  for _, item in pairs(inventory) do
+    local item_id = get_item_id(item)
+    if not costs[item_id] then
+      -- If the item is not in the recipe, we can't craft anything.
+      return 0
     end
+    current_items[item_id] = (current_items[item_id] or 0) + item.count
   end
 
-  ---@TODO Determine count of each item needed based on the recipe
-  ---@TODO Check if the turtle has enough of each item (multiply by count)
+  -- Calculate the maximum number of items we can craft, based on what is in the inventory currently.
+  local max_craftable = math.huge
+  for item_id, required_count in pairs(costs) do
+    local available_count = current_items[item_id] or 0
+    max_craftable = math.min(max_craftable, math.floor(available_count / required_count))
+  end
+  if max_craftable <= 0 then
+    return 0 -- Not enough items to craft anything.
+  end
+
+  if count and max_craftable < count then
+    if only_craft_count then
+      return 0 -- Not enough items to craft the requested amount.
+    else
+      count = max_craftable -- Adjust to the maximum we can craft.
+    end
+  elseif not count then
+    count = max_craftable -- Craft as many as possible.
+  end
+
+
+
   ---@TODO Check if any additional items are in the turtle's inventory (return 0)
   ---@TODO Craft.
 end
@@ -396,10 +501,15 @@ end
 ---@return integer crafted The number of items successfully crafted.
 function Inventory.interfacedCraft(recipe, count, interface)
   expect(1, recipe, "table")
+  validate_recipe(recipe)
   expect(2, count, "number", "nil")
   expect(3, interface, "table")
 
-  ---@TODO This entire method.
+  if interface.isClosed() then
+    error("Interface is closed.", 2)
+  end
+
+  local costs = Inventory.getRecipeCosts(recipe)
 end
 
 
