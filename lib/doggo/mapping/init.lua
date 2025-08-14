a3d99fd492765f2772c6f0bb95173351f1182541
Matthew W <fatboychummy@gmail.com>
2025-08-14 00:16:47 -0600

    Add initial init and storage modules for Doggo Mapping

---
+++ lib/doggo/mapping/init.lua
@@ -0,0 +1,12 @@
+--- Mapping module for Doggo
+
+local expect = require "cc.expect".expect
+
+---@class Doggo.Mapping
+local Mapping = {}
+
+
+---@TODO Link everything.
+
+
+return Mapping
---
+++ lib/doggo/mapping/storage.lua
@@ -0,0 +1,4 @@
+--- Storage module for Doggo mapping.
+--- Handles saving and loading maps.
+
+---@TODO "Large" and "Small" maps, containing block names and no block names, respectively.
